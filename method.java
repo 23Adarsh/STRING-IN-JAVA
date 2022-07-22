d79ddd2b681c8881595cc0aef03a7be804f58aa9
Adarsh Kumar Singh <107578738+23Adarsh@users.noreply.github.com>
2022-07-23 01:32:18 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,24 @@
+public class method{
+    public static void main(String[] args)
+    {
+        String a="RAHUL";
+        String b ="david";
+        
+        System.out.println(a.toLowerCase());
+        System.out.println(b.toUpperCase());
+        
+        System.out.println(a.concat(b));
+        System.out.println(a.length());
+        
+        String c ="  Sachin  ";
+        String d=" ";
+        
+        System.out.println(c.trim());
+        System.out.println(a.isEmpty());
+
+        System.out.println(a.charAt(2));
+        System.out.println(b.indexOf('v'));
+        
+    }
+} 
+
