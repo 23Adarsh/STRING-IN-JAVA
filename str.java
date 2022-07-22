e93f0444883baa0e6c6566547525ec568b71ac29
Adarsh Kumar Singh <107578738+23Adarsh@users.noreply.github.com>
2022-07-23 01:33:18 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,13 @@
+public class str {
+    public static void main(String[] args)
+    {
+        String s ="adarsh";
+        String b ="adarsh";
+        s.concat("Singh");
+
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(s);
+    }
+    // String is a inmuatable in java so any value declare then not change them. 
+}
